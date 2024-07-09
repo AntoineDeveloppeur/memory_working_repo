08c98df31ff86cd2b8ccfe84dbaeb2d7dd3cd12f
Antoine V <antoine.verove@gmail.com>
2024-07-09 08:55:08 +0200

    suppression du format json des cartes piques, elles prenaient trop de places et ne permettait pas d'avancer séreinement sur la mise en forme et les médias queries

--- scripts/options.js
+++ scripts/options.js
@@ -1,16 +1,19 @@
-
-import {listeTypeDeCarte, listeTypeDosDeCarte, recupererDonneesCartes} from "./jeu.js"
+import {
+    listeTypeDeCarte,
+    listeTypeDosDeCarte,
+    recupererDonneesCartes,
+} from './jeu.js'
 /**
  * @param {Number} p est la position dans le carroussel de choix du type de carte
-**/
-let p=0
+ **/
+let p = 0
 
-export function ouvrirLesOptions () {
-    if (document.querySelector(".grid3")) {
-        const bouttonOptions = document.querySelector(".grid3")
-        const popUpBackground = document.querySelector(".popUpBackground")
+export function ouvrirLesOptions() {
+    if (document.querySelector('.grid3')) {
+        const bouttonOptions = document.querySelector('.grid3')
+        const popUpBackground = document.querySelector('.popUpBackground')
         bouttonOptions.onclick = () => {
-            popUpBackground.classList.add("active")
+            popUpBackground.classList.add('active')
         }
         afficherNomTypedeCartes()
         creerContenuCarroussel()
@@ -20,103 +23,114 @@ export function ouvrirLesOptions () {
     }
 }
 
-function creerContenuCarroussel () {
-    recupererDonneesCartes ()
-    let listeCarteCarroussel = document.getElementById("liste-carte-carroussel")
-    listeCarteCarroussel.innerHTML=""
-    for (let i=0; i< listeTypeDeCarte.length; i++) {
-        const imageChoix = document.createElement("img")
+function creerContenuCarroussel() {
+    recupererDonneesCartes()
+    let listeCarteCarroussel = document.querySelector(
+        '.popUpBackground__options__carroussel__container__liste-cartes'
+    )
+    listeCarteCarroussel.innerHTML = ''
+    for (let i = 0; i < listeTypeDeCarte.length; i++) {
+        const imageChoix = document.createElement('img')
         imageChoix.src = listeTypeDeCarte[i].image1
-        imageChoix.classList.add("carte-carroussel")
+        imageChoix.classList.add(
+            'popUpBackground__options__carroussel__container__liste-cartes__carte'
+        )
         listeCarteCarroussel.appendChild(imageChoix)
     }
 }
 function afficherDifficulté() {
-    recupererDonneesCartes ()
-    let nombreEtoiles = document.getElementById("nombre-etoiles")
-    nombreEtoiles.innerHTML = ""
+    recupererDonneesCartes()
+    let nombreEtoiles = document.querySelector(
+        '.popUpBackground__options__carroussel__container__etoiles'
+    )
+    nombreEtoiles.innerHTML = ''
     // nombreEtoiles.innerText = listeTypeDeCarte[p].difficulte
-    for (let i = 0; i< listeTypeDeCarte[p].difficulte; i++) {
-        const etoileJaune = document.createElement("img")
-        etoileJaune.src = "images/star_1.svg"
-        etoileJaune.classList.add("etoile")
-        etoileJaune.alt = "etoile jaune de notation"
+    for (let i = 0; i < listeTypeDeCarte[p].difficulte; i++) {
+        const etoileJaune = document.createElement('img')
+        etoileJaune.src = 'images/star_1.svg'
+        etoileJaune.classList.add('etoile')
+        etoileJaune.alt = 'etoile jaune de notation'
 
         nombreEtoiles.appendChild(etoileJaune)
     }
     while (nombreEtoiles.childElementCount < 5) {
-        const etoileVide = document.createElement("img")
-        etoileVide.src = "images/star_0.svg"
-        etoileVide.classList.add("etoile")
-        etoileVide.alt = "etoile vide de notation"
+        const etoileVide = document.createElement('img')
+        etoileVide.src = 'images/star_0.svg'
+        etoileVide.classList.add('etoile')
+        etoileVide.alt = 'etoile vide de notation'
         nombreEtoiles.appendChild(etoileVide)
     }
 }
 function afficherNomTypedeCartes() {
     recupererDonneesCartes()
-    let container =  document.querySelector(`#container > :first-child`)
-   container.innerHTML = listeTypeDeCarte[p].nom
+    let container = document.querySelector(
+        `.popUpBackground__options__carroussel__container > :first-child`
+    )
+    container.innerHTML = listeTypeDeCarte[p].nom
 }
 
-function fermerLesOptions () {
-    const popUpBackground = document.querySelector(".popUpBackground")
-    popUpBackground.onclick=(event) => {
+function fermerLesOptions() {
+    const popUpBackground = document.querySelector('.popUpBackground')
+    popUpBackground.onclick = (event) => {
         if (event.target === popUpBackground) {
-            popUpBackground.classList.remove("active")
+            popUpBackground.classList.remove('active')
         }
     }
-    const valider = document.getElementById("valider")
-    valider.onclick=(event) => {
-        popUpBackground.classList.remove("active")
-        validerChoixTypeDeCartes ()
-        
+    const valider = document.querySelector(
+        '.popUpBackground__options__carroussel__container__valider'
+    )
+    valider.onclick = (event) => {
+        popUpBackground.classList.remove('active')
+        validerChoixTypeDeCartes()
     }
 }
-function defilerLeCarroussel () {
-    const listeCarteCarroussel = document.getElementById("liste-carte-carroussel")
-    listeCarteCarroussel.style.transition = "all 0.5s ease"
-    const flecheGauche = document.getElementById("gauche")
-    flecheGauche.onclick=function() {
-        if (p>0) {
+function defilerLeCarroussel() {
+    const listeCarteCarroussel = document.querySelector(
+        '.popUpBackground__options__carroussel__container__liste-cartes'
+    )
+    listeCarteCarroussel.style.transition = 'all 0.5s ease'
+    const flecheGauche = document.querySelector('.fleche--gauche')
+    flecheGauche.onclick = function () {
+        if (p > 0) {
             p--
             // listeCarteCarroussel.style.transform="transform("p*800+"px)"
-            listeCarteCarroussel.style.transform = "translate("+-336*p+"px)" 
+            listeCarteCarroussel.style.transform =
+                'translate(' + -336 * p + 'px)'
             afficherMasquerBouttons()
             afficherNomTypedeCartes()
             afficherDifficulté()
         }
     }
-    const flecheDroite = document.getElementById("droite")
-    flecheDroite.onclick=function() {
-        if (p<3) {
+    const flecheDroite = document.querySelector('.fleche--droite')
+    flecheDroite.onclick = function () {
+        if (p < 3) {
             p++
             // listeCarteCarroussel.style.transform="transform("p*800+"px)"
-            listeCarteCarroussel.style.transform="translate("+-336*p+"px)"
+            listeCarteCarroussel.style.transform =
+                'translate(' + -336 * p + 'px)'
             afficherMasquerBouttons()
             afficherNomTypedeCartes()
             afficherDifficulté()
         }
     }
-    
 }
 
 function afficherMasquerBouttons() {
-    recupererDonneesCartes ()
-    const flecheDroite = document.getElementById("droite")
-    if(p==listeTypeDeCarte.length-1) {
-        flecheDroite.style.visibility = "hidden"
+    recupererDonneesCartes()
+    const flecheDroite = document.querySelector('.fleche--droite')
+    if (p == listeTypeDeCarte.length - 1) {
+        flecheDroite.style.visibility = 'hidden'
     } else {
-        flecheDroite.style.visibility = "visible"
+        flecheDroite.style.visibility = 'visible'
     }
-    const flecheGauche = document.getElementById("gauche")
-    if(p==0) {
-        flecheGauche.style.visibility = "hidden"
+    const flecheGauche = document.querySelector('.fleche--gauche')
+    if (p == 0) {
+        flecheGauche.style.visibility = 'hidden'
     } else {
-        flecheGauche.style.visibility = "visible"
+        flecheGauche.style.visibility = 'visible'
     }
 }
 
-function validerChoixTypeDeCartes () {
-    window.localStorage.setItem("ChoixTypeDeCartes",p)
-    
+function validerChoixTypeDeCartes() {
+    window.localStorage.setItem('ChoixTypeDeCartes', p)
 }
